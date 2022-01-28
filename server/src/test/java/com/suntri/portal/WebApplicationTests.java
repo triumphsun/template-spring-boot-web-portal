5d06f6244ce6194757e6f47a51229fd895f69b63
Chia-Yang Sun <triumph.sun@gmail.com>
2022-01-28 20:49:32 +0800

    add spring-boot support - web.

---
+++ server/src/test/java/com/suntri/portal/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suntri.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
