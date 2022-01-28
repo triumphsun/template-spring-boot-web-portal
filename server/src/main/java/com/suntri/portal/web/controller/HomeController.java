ce037c0c44a1e06db8e56fcf85f7037408026c0b
Chia-Yang Sun <triumph.sun@gmail.com>
2022-01-28 21:15:23 +0800

    add spring-boot support - thymeleaf.

---
+++ server/src/main/java/com/suntri/portal/web/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.suntri.portal.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(
+        path = {"/"},
+        method = {RequestMethod.GET}
+    )
+    public ModelAndView home(){
+        return new ModelAndView("home");
+    }
+
+}
